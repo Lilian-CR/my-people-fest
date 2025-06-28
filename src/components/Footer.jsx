c8e7feb64a5c01460e16f958fb2ab0e518b185a2
Lilian-CR <lilianxxx@gmail.com>
2025-06-29 01:46:53 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="bg-[#221313] text-white text-sm py-2 px-4">
+      <footer className="bg-[#221313] text-white text-sm sticky py-2 px-4">
         <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4 text-center">
           {/* Social Icons */}
           <div className="flex gap-4 text-lg">
