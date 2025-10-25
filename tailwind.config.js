7d3cd01e56c9e867752d206577f1ecda504286a8
Lilian Rodrigues <lilianxxx@gmail.com>
2025-10-25 04:49:39 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         'mpf-white': '#F4F4F4',
         'mpf-beige': '#E1CFA5',
       },
+      fontFamily: {
+        morganite: ['Morganite', 'sans-serif'], // Added custom font
+      },
     },
   },
   variants: {
