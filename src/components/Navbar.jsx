9728059a341c1768998b3e6bc6d50eadb2793c72
Lilian Rodrigues <lilianxxx@gmail.com>
2025-11-23 09:09:27 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ import logo2026 from "../images/Logo_2026-transparent.png";
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  ];
-
   return (
     <header className="bg-black sticky top-0 z-50 w-full">
       )}
     </header>
   );
 }
+
 
 
 
